85b1a405cb236641a21bf8531acc8f400df76b49
velosovictor <veloso.assuncao.victor@gmail.com>
2022-12-12 12:09:17 -0300

    update

--- src/models/user.py
+++ src/models/user.py
@@ -32,3 +32,210 @@ class User(BaseModel):
     navio_parametro_24: float
     navio_parametro_25: float
 
+class Coordenadas(BaseModel):
+    coord_tipo_01: float
+    coord_xn_01: float
+    coord_yn_01: float
+    coord_zn_01: float
+    coord_xb_01: float
+    coord_yb_01: float
+    coord_zb_01: float
+
+    coord_tipo_02: float
+    coord_xn_02: float
+    coord_yn_02: float
+    coord_zn_02: float
+    coord_xb_02: float
+    coord_yb_02: float
+    coord_zb_02: float
+
+    coord_tipo_03: float
+    coord_xn_03: float
+    coord_yn_03: float
+    coord_zn_03: float
+    coord_xb_03: float
+    coord_yb_03: float
+    coord_zb_03: float
+
+    coord_tipo_04: float
+    coord_xn_04: float
+    coord_yn_04: float
+    coord_zn_04: float
+    coord_xb_04: float
+    coord_yb_04: float
+    coord_zb_04: float
+
+    coord_tipo_05: float
+    coord_xn_05: float
+    coord_yn_05: float
+    coord_zn_05: float
+    coord_xb_05: float
+    coord_yb_05: float
+    coord_zb_05: float
+
+    coord_tipo_06: float
+    coord_xn_06: float
+    coord_yn_06: float
+    coord_zn_06: float
+    coord_xb_06: float
+    coord_yb_06: float
+    coord_zb_06: float
+
+    coord_tipo_07: float
+    coord_xn_07: float
+    coord_yn_07: float
+    coord_zn_07: float
+    coord_xb_07: float
+    coord_yb_07: float
+    coord_zb_07: float
+
+    coord_tipo_08: float
+    coord_xn_08: float
+    coord_yn_08: float
+    coord_zn_08: float
+    coord_xb_08: float
+    coord_yb_08: float
+    coord_zb_08: float
+
+    coord_tipo_09: float
+    coord_xn_09: float
+    coord_yn_09: float
+    coord_zn_09: float
+    coord_xb_09: float
+    coord_yb_09: float
+    coord_zb_09: float
+
+    coord_tipo_10: float
+    coord_xn_10: float
+    coord_yn_10: float
+    coord_zn_10: float
+    coord_xb_10: float
+    coord_yb_10: float
+    coord_zb_10: float
+
+    coord_tipo_11: float
+    coord_xn_11: float
+    coord_yn_11: float
+    coord_zn_11: float
+    coord_xb_11: float
+    coord_yb_11: float
+    coord_zb_11: float
+
+    coord_tipo_12: float
+    coord_xn_12: float
+    coord_yn_12: float
+    coord_zn_12: float
+    coord_xb_12: float
+    coord_yb_12: float
+    coord_zb_12: float
+
+    coord_tipo_13: float
+    coord_xn_13: float
+    coord_yn_13: float
+    coord_zn_13: float
+    coord_xb_13: float
+    coord_yb_13: float
+    coord_zb_13: float
+
+    coord_tipo_14: float
+    coord_xn_14: float
+    coord_yn_14: float
+    coord_zn_14: float
+    coord_xb_14: float
+    coord_yb_14: float
+    coord_zb_14: float
+
+    coord_tipo_15: float
+    coord_xn_15: float
+    coord_yn_15: float
+    coord_zn_15: float
+    coord_xb_15: float
+    coord_yb_15: float
+    coord_zb_15: float
+
+    coord_tipo_16: float
+    coord_xn_16: float
+    coord_yn_16: float
+    coord_zn_16: float
+    coord_xb_16: float
+    coord_yb_16: float
+    coord_zb_16: float
+
+class Coefvento(BaseModel):
+    coef_vento_cx_000: float
+    coef_vento_cy_000: float
+    coef_vento_cn_000: float
+
+    coef_vento_cx_100: float
+    coef_vento_cy_100: float
+    coef_vento_cn_100: float
+
+    coef_vento_cx_010: float
+    coef_vento_cy_010: float
+    coef_vento_cn_010: float
+
+    coef_vento_cx_110: float
+    coef_vento_cy_110: float
+    coef_vento_cn_110: float
+
+    coef_vento_cx_020: float
+    coef_vento_cy_020: float
+    coef_vento_cn_020: float
+
+    coef_vento_cx_120: float
+    coef_vento_cy_120: float
+    coef_vento_cn_120: float
+
+    coef_vento_cx_030: float
+    coef_vento_cy_030: float
+    coef_vento_cn_030: float
+
+    coef_vento_cx_130: float
+    coef_vento_cy_130: float
+    coef_vento_cn_130: float
+
+    coef_vento_cx_040: float
+    coef_vento_cy_040: float
+    coef_vento_cn_040: float
+
+    coef_vento_cx_140: float
+    coef_vento_cy_140: float
+    coef_vento_cn_140: float
+
+    coef_vento_cx_050: float
+    coef_vento_cy_050: float
+    coef_vento_cn_050: float
+
+    coef_vento_cx_150: float
+    coef_vento_cy_150: float
+    coef_vento_cn_150: float
+
+    coef_vento_cx_060: float
+    coef_vento_cy_060: float
+    coef_vento_cn_060: float
+
+    coef_vento_cx_160: float
+    coef_vento_cy_160: float
+    coef_vento_cn_160: float
+
+    coef_vento_cx_070: float
+    coef_vento_cy_070: float
+    coef_vento_cn_070: float
+
+    coef_vento_cx_170: float
+    coef_vento_cy_170: float
+    coef_vento_cn_170: float
+
+    coef_vento_cx_080: float
+    coef_vento_cy_080: float
+    coef_vento_cn_080: float
+
+    coef_vento_cx_180: float
+    coef_vento_cy_180: float
+    coef_vento_cn_180: float
+
+    coef_vento_cx_090: float
+    coef_vento_cy_090: float
+    coef_vento_cn_090: float
+
+
